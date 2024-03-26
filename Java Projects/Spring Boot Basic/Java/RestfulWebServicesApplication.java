b4f1305fcc81e2f596676db579302a8bfa233f41
Rhnsen2512 <89203764+Rhnsen2512@users.noreply.github.com>
2024-03-26 20:22:15 +0530

    Add files via upload

---
+++ Java Projects/Spring Boot Basic/Java/RestfulWebServicesApplication.java
@@ -0,0 +1,14 @@
+package com.rohan.rest.webservices.RestfulWebServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+}
+
