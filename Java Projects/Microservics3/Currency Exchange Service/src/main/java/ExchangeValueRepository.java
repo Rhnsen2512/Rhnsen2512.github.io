2c278d4a5c28c77f9c0322858a2796295ecbc1fc
Rhnsen2512 <89203764+Rhnsen2512@users.noreply.github.com>
2024-10-30 11:10:25 +0530

    Add files via upload

---
+++ Java Projects/Microservics3/Currency Exchange Service/src/main/java/ExchangeValueRepository.java
@@ -0,0 +1,9 @@
+package com.rohan.microservices.currency_exchange_service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+	
+	ExchangeValue findByFromAndTo(String from, String to);
+
+}
